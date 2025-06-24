24192f38e04cf41e526dfe360947ebea6180f34a
Kajal <kajal.singhai@mindcrewtech.com>
2025-06-24 13:30:19 +0530

    build update

--- .next/types/app/dashboard/page.ts
+++ .next/types/app/dashboard/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/muskan/Documents/wellnex/app/dashboard/page.tsx
+// File: D:\Kajal\wellnex\wellnex-FE\app\dashboard\page.tsx
 import * as entry from '../../../../app/dashboard/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
