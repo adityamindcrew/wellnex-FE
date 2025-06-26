d9ec1053e07e26498ac9b3e8e29d2d1937cc670b
Kajal <kajal.singhai@mindcrewtech.com>
2025-06-26 18:13:13 +0530

    testing env's

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation"
 
 export default function Home() {
+  console.log('API URL:', process.env.NEXT_PUBLIC_API_URL);
   redirect("/landing")
   return null;
 }
